f0e076832d793e6202131107e76459a2a7fc0b1e
Dasho <git@dasho.dev>
2023-10-28 07:22:30 +0100

    fix(core #5): The API can't use (read) from a dotenv file.
    Go gotted github.com/joho/godotenv and loaded dotenv library to load dotenv file secrets as variables.

--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 
 require (
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 )
--- main.go
+++ main.go
@@ -4,19 +4,35 @@ import (
 	"api/src/server"
 	"log"
 	"os"
+
+	dotenv "github.com/joho/godotenv"
 )
 
 func main() {
+	// Load the .env file
+	err := dotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err == nil {
+		log.Println("Loaded .env file")
+	}
+
 	// Get the port from the env file
-	port := os.Getenv("PORT")
+	port := os.Getenv("SDS_API_PORT")
+	host := os.Getenv("SDS_API_HOST")
 
 	// If the port is empty, set it to 8080
 	if port == "" {
 		port = "8080"
 	}
 
+	if host == "" {
+		host = "127.0.0.1"
+	}
+
 	// Create a new server
-	s := server.NewServer("127.0.0.1:"+port, port)
+	s := server.NewServer(host+":"+port, port)
 
 	// Start the server
 	log.Fatal(s.Start())
