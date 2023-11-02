c53e0f1a88519ac14c39bb7ed3e68256b200ab80
Photon862 <elias@st.ax>
2023-11-02 10:06:14 +0800

    v0.0.6
    mid progress push
    still position sticky left

--- src/pages/landing/prelaunch/PreLaunch.js
+++ src/pages/landing/prelaunch/PreLaunch.js
@@ -37,7 +37,7 @@ export default function Index() {
   });
 
   document.documentElement.setAttribute("data-apptheme", "dark");
-  document.body.style.overflow = 'auto';
+  document.body.style.overflow = 'initial';
 
   var logo = "https://cdn.st.ax/v2/logo.svg";
 
@@ -64,16 +64,180 @@ export default function Index() {
 
   });
 
+  // Utility function
+function Util () {};
+
+Util.is = function(elem, selector) {
+  if(selector.nodeType){
+    return elem === selector;
+  }
+
+  var qa = (typeof(selector) === 'string' ? document.querySelectorAll(selector) : selector),
+    length = qa.length,
+    returnArr = [];
+
+  while(length--){
+    if(qa[length] === elem){
+      return true;
+    }
+  }
+
+  return false;
+};
+
+// Check if Reduced Motion is enabled
+Util.osHasReducedMotion = function() {
+  if(!window.matchMedia) return false;
+  var matchMediaObj = window.matchMedia('(prefers-reduced-motion: reduce)');
+  if(matchMediaObj) return matchMediaObj.matches;
+  return false; // return false if not supported
+};
+
+// Tutorial - https://codyhouse.co/tutorials/how-stacking-cards
+
+useEffect(() => {
+  var StackCards = function(element) {
+    this.element = element;
+    this.items = this.element.getElementsByClassName(css["landing-updates-box"]);
+    this.scrollingFn = false;
+    this.scrolling = false;
+    initStackCardsEffect(this);
+    // initStackCardsResize(this);
+  };
+
+  function initStackCardsEffect(element) { // use Intersection Observer to trigger animation
+    setStackCards(element); // store cards CSS properties
+    var observer = new IntersectionObserver(stackCardsCallback.bind(element), { threshold: [0, 1] });
+    observer.observe(element.element);
+  };
+
+  function stackCardsCallback(entries) { // Intersection Observer callback
+    if(entries[0].isIntersecting) {
+      if(this.scrollingFn) return; // listener for scroll event already added
+      stackCardsInitEvent(this);
+    } else {
+      if(!this.scrollingFn) return; // listener for scroll event already removed
+      window.removeEventListener('scroll', this.scrollingFn);
+      this.scrollingFn = false;
+    }
+  };
+
+  function stackCardsInitEvent(element) {
+    element.scrollingFn = stackCardsScrolling.bind(element);
+    window.addEventListener('scroll', element.scrollingFn);
+  };
+
+  function stackCardsScrolling() {
+    if(this.scrolling) return;
+    this.scrolling = true;
+    window.requestAnimationFrame(animateStackCards.bind(this));
+  };
+
+  function setStackCards(element) {
+    // store wrapper properties
+    element.marginY = getComputedStyle(element.element).getPropertyValue('--stack-cards-gap');
+    getIntegerFromProperty(element); // convert element.marginY to integer (px value)
+    element.elementHeight = element.element.offsetHeight;
+
+    // store card properties
+    var cardStyle = getComputedStyle(element.items[0]);
+    element.cardTop = Math.floor(parseFloat(cardStyle.getPropertyValue('top')));
+    element.cardHeight = Math.floor(parseFloat(cardStyle.getPropertyValue('height')));
+
+    // store window property
+    element.windowHeight = window.innerHeight;
+
+    // reset margin + translate values
+    if(isNaN(element.marginY)) {
+      element.element.style.paddingBottom = '0px';
+    } else {
+      element.element.style.paddingBottom = (element.marginY*(element.items.length - 1))+'px';
+    }
+
+    for(var i = 0; i < element.items.length; i++) {
+      if(isNaN(element.marginY)) {
+        element.items[i].style.transform = 'none;';
+      } else {
+        element.items[i].style.transform = 'translateY('+element.marginY*i+'px)';
+      }
+    }
+  };
+
+  function getIntegerFromProperty(element) {
+    var node = document.createElement('div');
+    node.setAttribute('style', 'opacity:0; visbility: hidden;position: absolute; height:'+element.marginY);
+    element.element.appendChild(node);
+    element.marginY = parseInt(getComputedStyle(node).getPropertyValue('height'));
+    element.element.removeChild(node);
+  };
+
+  function animateStackCards() {
+    if(isNaN(this.marginY)) { // --stack-cards-gap not defined - do not trigger the effect
+      this.scrolling = false;
+      return;
+    }
+
+    var top = this.element.getBoundingClientRect().top;
+
+    if( this.cardTop - top + this.element.windowHeight - this.elementHeight - this.cardHeight + this.marginY + this.marginY*this.items.length > 0) {
+      this.scrolling = false;
+      return;
+    }
+
+    for(var i = 0; i < this.items.length; i++) { // use only scale
+      var scrolling = this.cardTop - top - i*(this.cardHeight+this.marginY);
+      if(scrolling > 0) {
+        var scaling = i == this.items.length - 1 ? 1 : (this.cardHeight - scrolling*0.05)/this.cardHeight;
+        this.items[i].style.transform = 'translateY('+this.marginY*i+'px) scale('+scaling+')';
+      } else {
+        this.items[i].style.transform = 'translateY('+this.marginY*i+'px)';
+      }
+    }
+
+    this.scrolling = false;
+  };
+
+  // initialize StackCards object
+  var stackCards = document.getElementsByClassName(css["landing-updates"]),
+    intersectionObserverSupported = ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype),
+    reducedMotion = Util.osHasReducedMotion();
+
+  if(stackCards.length > 0 && intersectionObserverSupported && !reducedMotion) {
+    var stackCardsArray = [];
+    for(var i = 0; i < stackCards.length; i++) {
+      (function(i){
+        stackCardsArray.push(new StackCards(stackCards[i]));
+      })(i);
+    }
+
+    var resizingId = false,
+      customEvent = new CustomEvent('resize-stack-cards');
+
+    window.addEventListener('resize', function() {
+      clearTimeout(resizingId);
+      resizingId = setTimeout(doneResizing, 500);
+    });
+
+    function doneResizing() {
+      for( var i = 0; i < stackCardsArray.length; i++) {
+        (function(i){stackCardsArray[i].element.dispatchEvent(customEvent)})(i);
+      };
+    };
+  }
+});
 
 
 
   return (
-    <div id="Index-react-div" className={cssGlobal["react-div"]}>
-      <div id="Index-react-background" className={cssGlobal["react-background"]}>
+    <div style={{overflow: "initial"}}  id="Index-react-div" className={cssGlobal["react-div"]}>
+      <div style={{overflow: "initial"}} id="Index-react-background" className={cssGlobal["react-background"]}>
          {/* style={{ backgroundImage: `url(${background})` }}> */}
-        <div className={cssGlobal["page-full"]}>
+        <div style={{overflow: "initial"}} className={cssGlobal["page-full"]}>
           <Addons />
           <Bubbles />
+          <div className={css["test"]}>
+            hi
+          </div>
           <div id="logo-display" className={`${css["logo-display"]} ${cssGlobal["flex-center-center"]}`}>
             <div className={css["logo-display-box"]}>
               <img src={logo} alt="logo" />
